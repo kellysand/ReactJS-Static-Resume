ef3e1bf8a0c41f1a7442f1dffd21788ee7f88366
kellysand <kellyxsand@gmail.com>
2024-09-13 12:39:56 +0200

    n

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"https://github.com/kellysand/ReactJS-Static-Resume",
+  base:"/",
   test: {
     globals: true,
     environment: 'jsdom',
