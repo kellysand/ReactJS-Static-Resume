91826ade3c9947e71c7fe9e2d48d145115a7dd99
kellysand <kellyxsand@gmail.com>
2024-09-17 11:47:39 +0200

    insert

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header";
+
+import BodyCom from "./components/body";
+function App() {
+  return (
+    <div className="App">
+    <Header></Header>
+    <BodyCom></BodyCom>
+    </div>
+  );
+}
+
+export default App;
